64b14d8192345ec9400d58d1e4172a8a92c910ca
DINESHKUMAR P <142890939+dineshdev13@users.noreply.github.com>
2026-02-05 16:51:04 +0530

    Update download links for movie entries

--- src/data/movies.js
+++ src/data/movies.js
@@ -7,7 +7,7 @@ export const movies = [
         description: "A light-hearted supernatural story where a man’s life changes after an unexpected encounter with a ghost, blending humor, emotion, and mystery.",
         cast: "Nivin Pauly, Riya Shibu, Aju Varghese, Janardhanan, Preity Mukhundhan",
         posterUrl: "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/sarvam-maya-et00473343-1766390933.jpg",
-        downloadLink: "https://drive.google.com/file/d/1ZhbCdk727qXjw350nULs1XA_ORCRRg5_/view?usp=drive_link" // Replace with Drive Link
+        downloadLink: "https://drive.google.com/drive/folders/1y-dzZvsjGxVtyU4QtjdJP0ZAN2axq-x6?usp=drive_link" // Replace with Drive Link
         
     },
     {
@@ -18,7 +18,7 @@ export const movies = [
     description: "A tense escort mission turns into a high-stakes battle of survival, exposing the system’s cracks and a man’s fight for justice.",
     cast: "Vikram Prabhu, LK Akshay Kumar, Anishma Anilkumar, Ananda Thambirajah, Harishankar Narayanan, Remya Suresh, Siju Wilson, Munnar Ramesh, P. L. Thenappan",
     posterUrl: "https://m.media-amazon.com/images/M/MV5BZTU0ZjE1ZTktMmQ4Ni00NThlLWI5OTUtNzMxMWEzMDYyZWY5XkEyXkFqcGc@._V1_.jpg",
-    downloadLink: "https://drive.google.com/file/d/YOUR_DRIVE_LINK/view?usp=drive_link" // Replace with Drive Link
+    downloadLink: "https://drive.google.com/drive/folders/1y-dzZvsjGxVtyU4QtjdJP0ZAN2axq-x6?usp=drive_link" // Replace with Drive Link
     },
     {
     id: 3,
@@ -28,7 +28,7 @@ export const movies = [
     description: "An intense romantic musical that explores love, longing, and emotional sacrifice, set against a deeply poetic and musical narrative.",
     cast: "Dhanush, Kriti Sanon",
     posterUrl: "https://m.media-amazon.com/images/M/MV5BOGRjMzM1ZmUtMjk0Yi00NzA0LTk3ZWYtZWM3MWY3M2EwMjBhXkEyXkFqcGc@._V1_.jpg",
-    downloadLink: "https://drive.google.com/file/d/19Gja76_a-tl888RTzR8w1F9eSGSBMxGy/view?usp=drive_link" // Replace with Drive Link
+    downloadLink: "https://drive.google.com/drive/folders/1y-dzZvsjGxVtyU4QtjdJP0ZAN2axq-x6?usp=drive_link" // Replace with Drive Link
 }
 
 
